c8952c762b506bad2c16481051d7a681d74bf074
Scott Kurz <skurz@us.ibm.com>
2023-12-09 13:38:04 -0500

    init commit
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

---
+++ src/main/java/com/example/sbinitmvnwar/SbInitMvnWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbinitmvnwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInitMvnWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInitMvnWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbinitmvnwar/SbInitMvnWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbinitmvnwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbInitMvnWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
